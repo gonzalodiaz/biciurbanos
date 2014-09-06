8cfd093f1269191d82d570234225716976f04a7f
Gonzalo Diaz <gonzalo@bytelion.com>
2014-09-06 12:19:33 -0300

    django app setup

--- biciurbanos/biciurbanos/settings.py
+++ biciurbanos/biciurbanos/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'app'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'PAGINATE_BY': 10
+}
